c9d03aad601fa7738e205b6353a185bfd74e95d7
Cas <153147743+CasYuY@users.noreply.github.com>
2025-07-18 23:55:40 +0800

    wip

--- app.js
+++ app.js
@@ -1,11 +1,56 @@
 import "dotenv/config";
-import routes from "./routes/index.js";
 import express from "express";
+import {
+  InteractionResponseFlags,
+  InteractionResponseType,
+  InteractionType,
+  MessageComponentTypes,
+  verifyKeyMiddleware,
+} from "discord-interactions";
+import { getRandomEmoji } from "./utils/utils.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(routes);
+app.post(
+  "/interactions",
+  verifyKeyMiddleware(process.env.PUBLIC_KEY),
+  async function (req, res) {
+    const { type, data } = req.body;
+
+    if (type === InteractionType.PING) {
+      return res.send({ type: InteractionResponseType.PONG });
+    }
+
+    if (type === InteractionType.APPLICATION_COMMAND) {
+      const { name } = data;
+
+      // "test" command
+      if (name === "test") {
+        // Send a message into the channel where command was triggered from
+        return res.send({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            flags: InteractionResponseFlags.IS_COMPONENTS_V2,
+            components: [
+              {
+                type: MessageComponentTypes.TEXT_DISPLAY,
+                // Fetches a random emoji to send from a helper function
+                content: `test v1.10 translation ${getRandomEmoji()}`,
+              },
+            ],
+          },
+        });
+      }
+
+      console.error(`unknown command: ${name}`);
+      return res.status(400).json({ error: "unknown command" });
+    }
+
+    console.error("unknown interaction type", type);
+    return res.status(400).json({ error: "unknown interaction type" });
+  },
+);
 
 app.listen(PORT, () => {
   console.log("Listening on port", PORT);
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import interactions from "./interactions.js";
-
-const router = Router();
-
-router.use("/interactions", interactions);
-
-export default router;
